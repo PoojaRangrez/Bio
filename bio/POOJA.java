ec9849b07e95cbfd414ea6713031749cc4362683
PoojaRangrez <89906169+PoojaRangrez@users.noreply.github.com>
2021-09-21 09:08:43 +0530

    Add files via upload

---
+++ bio/POOJA.java
@@ -0,0 +1,20 @@
+package bio;
+
+public class POOJA {
+	public static void dob() {
+		System.out.println("23-OCT");
+	}
+	public static void edu() {
+		System.out.println("ENGINEERING GRADUATE");
+	}
+    public static void loc() {
+    	System.out.println("VIJAYANAGARA");
+    }
+	public static void main(String[] args) {
+		System.out.println("POOJA'S BIO:");
+		POOJA.dob();
+		POOJA.edu();
+		POOJA.loc();
+	}
+
+}
